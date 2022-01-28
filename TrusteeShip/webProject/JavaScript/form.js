5afadd9cccb1540c61a0c9b5163add15382de64e
Srijan Samridh <74289654+SrijanSamridh@users.noreply.github.com>
2022-01-28 05:31:33 +0530

    Add files via upload

---
+++ TrusteeShip/webProject/JavaScript/form.js
@@ -0,0 +1,10 @@
+//  STORING DATA
+// var str = '';
+// function myName(){
+//         var b = document.getElementById('user').value
+//         document.getElementById('user-s').innerHTML = b;
+//         str = b;
+// }
+//
+// var name = document.getElementById('name');
+// name.innerHTML = str;
---
+++ TrusteeShip/webProject/JavaScript/method.js
@@ -0,0 +1,31 @@
+/*
+window.onload= function (){
+            document.getElementById("box1").onclick = function checkbox(){
+                document.getElementById("anc").href="payment.html";
+                document.getElementById("box1").style.background = "#36b9f5";
+                document.getElementById("box1").style.boxShadow = "0px 4px 20px #36b9f5";
+                document.getElementById("box2").style.background = "rgb(0 0 0 / 52%)";
+                document.getElementById("box2").style.boxShadow = "none";
+                document.getElementById("box3").style.background = "rgb(0 0 0 / 52%)";
+                document.getElementById("box3").style.boxShadow = "none";
+            }
+            document.getElementById("box2").onclick = function checkbox(){
+                document.getElementById("anc").href="https://www.paypal.com/in/webapps/mpp/home?kid=p39982204948&gclid=EAIaIQobChMI2cm4p5W29QIVMJJmAh3iqAzHEAAYASAAEgIiwvD_BwE&gclsrc=aw.ds";
+                document.getElementById("box2").style.background = "#36b9f5";
+                document.getElementById("box2").style.boxShadow = "0px 4px 20px #36b9f5";
+                document.getElementById("box1").style.background = "rgb(0 0 0 / 52%)";
+                document.getElementById("box1").style.boxShadow = "none";
+                document.getElementById("box3").style.background = "rgb(0 0 0 / 52%)";
+                document.getElementById("box3").style.boxShadow = "none";
+            }
+            document.getElementById("box3").onclick = function checkbox(){
+                document.getElementById("anc").href="netBanking.html";
+                document.getElementById("box3").style.background = "#36b9f5";
+                document.getElementById("box3").style.boxShadow = "0px 4px 20px #36b9f5";
+                document.getElementById("box1").style.background = "rgb(0 0 0 / 52%)";
+                document.getElementById("box1").style.boxShadow = "none";
+                document.getElementById("box2").style.background = "rgb(0 0 0 / 52%)";
+                document.getElementById("box2").style.boxShadow = "none";
+            }
+
+        } */
